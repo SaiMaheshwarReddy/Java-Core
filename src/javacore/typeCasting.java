d0bf9dbd6badc6b3630fac49c90a0099e21c9d9c
SaiMaheshwar <maheshwarreddysai@gmail.com>
2021-03-13 15:50:30 +0000

    first commit

---
+++ src/javacore/typeCasting.java
@@ -0,0 +1,22 @@
+
+package javacore;
+
+
+
+public class typeCasting {
+
+	public static void main(String[] args) {
+		int a = 4;
+		
+		System.out.println("This is implicit conversion");
+		System.out.println("the value of a is " + a);
+		
+	
+		int c = a;
+		
+		System.out.println("This is character " + (char)a);
+
+	}
+	
+
+}
